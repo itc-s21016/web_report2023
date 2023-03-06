2f0fac46e8a6b3ee1244448859ea5b5ed69cca58
s21016 <s21016@std.it-college.ac.jp>
2023-03-06 13:31:11 +0900

    ホームページの作成

--- app/Controllers/Http/ReportsController.ts
+++ app/Controllers/Http/ReportsController.ts
@@ -1,3 +1,11 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class ReportsController {}
+export default class ReportsController {
+  public async index(ctx: HttpContextContract) {
+    const data = {
+      title: '届け出・証明書申請システム',
+      message: ''
+    }
+    return ctx.view.render('reports/index', data)
+  }
+}
