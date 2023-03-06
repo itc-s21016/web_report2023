c50172d08b6486401d762d7d88cd9cb0b22c64e0
s21016 <s21016@std.it-college.ac.jp>
2023-03-06 14:08:43 +0900

    ログアウト状態だとホームページに「ログインしてください」とメッセージが出るようにした

--- app/Controllers/Http/ReportsController.ts
+++ app/Controllers/Http/ReportsController.ts
@@ -4,7 +4,7 @@ export default class ReportsController {
   public async index(ctx: HttpContextContract) {
     const data = {
       title: '届け出・証明書申請システム',
-      message: ''
+      message: 'ログインしてください'
     }
     return ctx.view.render('reports/index', data)
   }
